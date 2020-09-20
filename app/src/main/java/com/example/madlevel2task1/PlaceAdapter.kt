866020572b5a22acb2d56f24c7c85fd36a54b3e1
Pieter <pieter.miedema@hva.nl>
2020-09-20 14:34:30 +0200

    finsihed

--- app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
+++ app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -1,8 +1,34 @@
 package com.example.madlevel2task1
 
-class PlaceAdapter extends RecyclerView {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
 
-    inner class ViewHolder {
+class PlaceAdapter(private val places: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
+    inner class ViewHolder(placeView: View) : RecyclerView.ViewHolder(placeView) {
+
+        val binding = ItemPlaceBinding.bind(placeView)
+
+        fun databind(place: Place) {
+            binding.imageView.setImageResource(place.imageResId)
+            binding.textView.text = place.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(places[position])
     }
 }
